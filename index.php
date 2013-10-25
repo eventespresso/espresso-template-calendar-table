805b2aff93368198eeb08b92452610b37afb6bab
Seth <seth@eventespresso.com>
2013-10-24 21:34:32 -0600

    Cleaning up some code and fixing the wait list button. [touch:3917]

--- index.php
+++ index.php
@@ -42,7 +42,7 @@ function espresso_custom_template_calendar_table(){
 	if(isset($ee_attributes['change_title'])) { $change_title = $ee_attributes['change_title']; }
 
 	//Load the css file
-	wp_register_style( 'espresso_cal_table_css', ESPRESSO_CUSTOM_DISPLAY_PLUGINPATH."/templates/calendar-table/style.css" );
+	wp_register_style( 'espresso_cal_table_css', WP_PLUGIN_URL. "/".plugin_basename(dirname(__FILE__)) .'/style.css' );
 	wp_enqueue_style( 'espresso_cal_table_css');
 
 	//Clears the month name
@@ -64,31 +64,34 @@ function espresso_custom_template_calendar_table(){
 			$event_meta			= unserialize($event->event_meta);
 			$externalURL 		= $event->externalURL;
 			$registration_url 	= !empty($externalURL) ? $externalURL : espresso_reg_url($event->id);
-			$live_button 		= '<a id="a_register_link-'.$event->id.'" href="'.$registration_url.'"><img class="buytix_button" src="'.ESPRESSO_CUSTOM_DISPLAY_PLUGINPATH.'/templates/calendar-table/register-now.png" alt="Buy Tickets"></a>';
-			$open_spots 		= apply_filters('filter_hook_espresso_get_num_available_spaces', $event->id);
+			$live_button 		= '<a id="a_register_link-'.$event->id.'" href="'.$registration_url.'"><img class="buytix_button" src="'.WP_PLUGIN_URL. "/".plugin_basename(dirname(__FILE__)) .'/register-now.png" alt="Buy Tickets"></a>';
+			//$open_spots 		= apply_filters('filter_hook_espresso_get_num_available_spaces', $event->id);
+			$open_spots			= get_number_of_attendees_reg_limit($event->id, 'number_available_spaces');
 			$featured_image		= isset($event_meta['event_thumbnail_url']) ? $event_meta['event_thumbnail_url'] : FALSE;
-
-			//This line changes the button text to display "Closed" if the attendee limit is reached.
-			if ( $open_spots < 1 || event_espresso_get_status($event->id) == 'NOT_ACTIVE' ) { $live_button = '<img class="buytix_button" src="'.ESPRESSO_CUSTOM_DISPLAY_PLUGINPATH.'/templates/calendar-table/closed.png" alt="Buy Tickets">';  }
-
-			//waitlist
-			if ($event->allow_overflow == 'Y' && event_espresso_get_status($event->id) == 'ACTIVE'){
-				$live_button = '<a href="'.espresso_reg_url($event->overflow_event_id).'"><img class="buytix_button" src="'.ESPRESSO_CUSTOM_DISPLAY_PLUGINPATH.'/templates/calendar-table/waiting.png" alt="Join Waiting List"></a>';
+			
+			if($open_spots < 1 && $event->allow_overflow == 'N') {
+				$live_button = '<img class="buytix_button" src="'.ESPRESSO_CUSTOM_DISPLAY_PLUGINPATH.'/templates/calendar-table/closed.png" alt="Closed">';
+			} else if ($open_spots < 1 && $event->allow_overflow == 'Y'){
+				$live_button = !empty($event->overflow_event_id) ? '<a href="'.espresso_reg_url($event->overflow_event_id).'"><img class="buytix_button" src="'.WP_PLUGIN_URL. "/".plugin_basename(dirname(__FILE__)) .'/waiting.png" alt="Join Waiting List"></a>' : '<img class="buytix_button" src="'.ESPRESSO_CUSTOM_DISPLAY_PLUGINPATH.'/templates/calendar-table/closed.png" alt="Closed">';
 			}
-
+				
+			if ( $event_status == 'NOT_ACTIVE' ) {
+				$live_button = '<img class="buytix_button" src="'.ESPRESSO_CUSTOM_DISPLAY_PLUGINPATH.'/templates/calendar-table/closed.png" alt="Closed">';
+			}
+				
 			//Build the table headers
 			$full_month = event_date_display($event->start_date, "F");
 			if ($temp_month != $full_month){
 				?>
-	<tr class="cal-header-month">
-		<th class="cal-header-month-name" id="calendar-header-<?php echo $full_month; ?>" colspan="3"><?php echo $full_month; ?></th>
-	</tr>
-	<tr class="cal-header">
-		<th><?php echo !isset($show_featured) || $show_featured === 'false' ? __('Date','event_espresso') :  '' ?></th>
-		<th class="th-event-info"><?php if(isset($change_title)) { echo $change_title; } else { _e('Band / Artist','event_espresso'); } ?></th>
-		<th class="th-tickets"><?php _e('Tickets','event_espresso'); ?></th>
-	</tr>
-	<?php
+				<tr class="cal-header-month">
+					<th class="cal-header-month-name" id="calendar-header-<?php echo $full_month; ?>" colspan="3"><?php echo $full_month; ?></th>
+				</tr>
+				<tr class="cal-header">
+					<th><?php echo !isset($show_featured) || $show_featured === 'false' ? __('Date','event_espresso') :  '' ?></th>
+					<th class="th-event-info"><?php if(isset($change_title)) { echo $change_title; } else { _e('Band / Artist','event_espresso'); } ?></th>
+					<th class="th-tickets"><?php _e('Tickets','event_espresso'); ?></th>
+				</tr>
+				<?php
 				$temp_month = $full_month;
 			}
 
@@ -101,35 +104,35 @@ function espresso_custom_template_calendar_table(){
 				event_espresso_user_login();
 			}else{
 				?>
-	<tr class="">
+		<tr class="">
 
 
-		<?php
-
-		if(isset($show_featured ) && $show_featured === 'true') { ?>
-			<td class="td-fet-image"><div class="">
-				<img src="<?php echo $featured_image; ?>" />
-			</div></td>
-		<?php } else { ?>
-		<td class="td-date-holder"><div class="dater">
-				<p class="cal-day-title"><?php echo event_date_display($event->start_date, "l"); ?></p>
-				<p class="cal-day-num"><?php echo event_date_display($event->start_date, "j"); ?></p>
-				<p><span><?php echo event_date_display($event->start_date, "M"); ?></span></p>
-			<?php } ?>
-			</div>
-		</td>
-
-		<td class="td-event-info"><span class="event-title"><a href="<?php echo $registration_url ?>"><?php echo stripslashes_deep($event->event_name); ?></a></span>
-			<p>
-				<?php _e('When:', 'event_espresso'); ?>
-				<?php echo event_date_display($event->start_date); ?><br />
-				<?php _e('Where:', 'event_espresso'); ?>
-				<?php echo stripslashes_deep($event->venue_address.', '.$event->venue_city.', '.$event->venue_state); ?><br />
-				<?php _e('Price: ', 'event_espresso'); ?>
-				<?php echo  $org_options['currency_symbol'].$event->event_cost; ?> </p>
-			<?php echo espresso_format_content(array_shift(explode('<!--more-->', $event->event_desc))); //Includes <p> tags ?></td>
-		<td class="td-event-register"><?php echo $live_button ?></td>
-	</tr>
+			<?php
+	
+			if(isset($show_featured ) && $show_featured === 'true') { ?>
+				<td class="td-fet-image"><div class="">
+					<img src="<?php echo $featured_image; ?>" />
+				</div></td>
+			<?php } else { ?>
+			<td class="td-date-holder"><div class="dater">
+					<p class="cal-day-title"><?php echo event_date_display($event->start_date, "l"); ?></p>
+					<p class="cal-day-num"><?php echo event_date_display($event->start_date, "j"); ?></p>
+					<p><span><?php echo event_date_display($event->start_date, "M"); ?></span></p>
+				<?php } ?>
+				</div>
+			</td>
+	
+			<td class="td-event-info"><span class="event-title"><a href="<?php echo $registration_url ?>"><?php echo stripslashes_deep($event->event_name); ?></a></span>
+				<p>
+					<?php _e('When:', 'event_espresso'); ?>
+					<?php echo event_date_display($event->start_date); ?><br />
+					<?php _e('Where:', 'event_espresso'); ?>
+					<?php echo stripslashes_deep($event->venue_address.', '.$event->venue_city.', '.$event->venue_state); ?><br />
+					<?php _e('Price: ', 'event_espresso'); ?>
+					<?php echo  $org_options['currency_symbol'].$event->event_cost; ?> </p>
+				<?php echo espresso_format_content(array_shift(explode('<!--more-->', $event->event_desc))); //Includes <p> tags ?></td>
+			<td class="td-event-register"><?php echo $live_button ?></td>
+		</tr>
 	<?php
 			}// close is_user_logged_in
 		 } //close foreach ?>
